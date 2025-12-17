5304e76d68bcabc6ab9702d0223942d892d60e02
shreyan-naskar <shreyannaskar@gmail.com>
2025-12-17 20:42:35 +0530

    .env added

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const userRouter = require('./routers/userRoutes');
 const app = express();
 
 // GLOBAL MIDDLEWARES
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+const dotenv = require('dotenv').config();
 const app = require('./app');
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server running on ${port}`);
