a4a571acceec7579e9995120f27b5af07ff62dd8
shreyan-naskar <shreyannaskar@gmail.com>
2025-12-26 13:12:49 +0530

    import new data

--- models/tourModel.js
+++ models/tourModel.js
@@ -78,6 +78,30 @@ const tourSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    startLocation: {
+      // GeoJSON for geospacial data: embedded object
+      type: {
+        type: String,
+        default: 'Point',
+        enum: ['Point'],
+      },
+      coordinates: [Number],
+      address: String,
+      description: String,
+    },
+    locations: [
+      {
+        type: {
+          type: String,
+          default: 'Point',
+          enum: ['Point'],
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number, // day when people will visit this location
+      },
+    ],
   },
   {
     toJSON: { virtuals: true },
