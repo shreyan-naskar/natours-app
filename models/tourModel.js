0a2dcec06ef1b69fcefb86997248442abdd83588
shreyan-naskar <shreyannaskar@gmail.com>
2025-12-27 18:55:46 +0530

    create indexes for read performance

--- models/tourModel.js
+++ models/tourModel.js
@@ -112,6 +112,9 @@ const tourSchema = new mongoose.Schema(
     toObject: { virtuals: true },
   },
 );
+// add custom indexes
+tourSchema.index({ price: 1, ratingsAverage: -1 }); // 1 for asc; -1 for desc
+tourSchema.index({ slug: 1 });
 
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
